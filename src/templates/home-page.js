8763904fdacf5852d91bb9d0f092757a0de75917
Marcin Jakubik <marcinjak9@gmail.com>
2018-08-25 17:39:26 +0200

    fixed homepage and formatted config.yml

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Content, { HTMLContent } from '../components/Content'
 
-export const HomePageTemplate = () => {
+export const HomePageTemplate = (props) => {
   return (
     <section className="section section--gradient">
-      lol
+      lol template
+      {console.log(props)}
     </section>
   )
 }
@@ -13,9 +14,10 @@ export const HomePageTemplate = () => {
 HomePageTemplate.propTypes = {
 }
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <HomePageTemplate
+      {...props}
       // contentComponent={HTMLContent}
       // title={post.frontmatter.title}
       // content={post.html}
@@ -29,13 +31,32 @@ HomePage.propTypes = {
 
 export default HomePage
 
-// export const homePageQuery = graphql`
-//   query AboutPage($id: String!) {
-//     markdownRemark(id: { eq: $id }) {
-//       html
-//       frontmatter {
-//         title
-//       }
-//     }
-//   }
-// `
+export const homePageQuery = graphql`
+  query AboutPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        heroSection {
+          heroBackgroundImage
+          heroPayoff
+          heroTitle
+          heroPayoff
+          HeroCtaServices {
+            HeroCtaServicesItem
+          }
+        }
+        homeDescriptionSection {
+          homeDescriptionTitle
+          homeFeatureColumn {
+            homeFeatureBody
+            homeFeatureCtaText
+            homeFeatureCtaUrl
+            homeFeatureIcon
+            homeFeatureTitle
+          }
+        }
+      }
+    }
+  }
+`
