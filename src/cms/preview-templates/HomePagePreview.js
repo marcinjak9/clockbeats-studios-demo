d941cf5bd581d19ff8625be114f9083a5ac8a6cf
Marcin Jakubik <marcinjak9@gmail.com>
2018-08-25 14:48:21 +0200

    homepage cms fix

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -4,16 +4,8 @@ import { HomePageTemplate } from '../../templates/home-page'
 
 const HomePagePreview = ({ entry, widgetFor }) => (
   <HomePageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
+
   />
 )
-
-HomePagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
 
 export default HomePagePreview
--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -2,58 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-
+export const HomePageTemplate = () => {
   return (
     <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
+      lol
     </section>
   )
 }
 
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+HomePageTemplate.propTypes = {
 }
 
-const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
-
+const HomePage = () => {
   return (
-    <AboutPageTemplate
-      contentComponent={HTMLContent}
-      title={post.frontmatter.title}
-      content={post.html}
+    <HomePageTemplate
+      // contentComponent={HTMLContent}
+      // title={post.frontmatter.title}
+      // content={post.html}
     />
   )
 }
 
-AboutPage.propTypes = {
+HomePage.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-export default AboutPage
+export default HomePage
 
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+// export const homePageQuery = graphql`
+//   query AboutPage($id: String!) {
+//     markdownRemark(id: { eq: $id }) {
+//       html
+//       frontmatter {
+//         title
+//       }
+//     }
+//   }
+// `
